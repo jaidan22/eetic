5fb25079804dc8d37604be595be0988340749d4b
jaidan22 <jaidansleeba22@gmail.com>
2022-02-12 09:55:04 +0530

    contact component

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./contact.scss";
+import homeBg from "../../img/map-imgaage.png";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="wrapper">
+        <h2>Welcome To Our Studio!</h2>
+        <h1>IT'S NICE TO MEET YOU</h1>
+        <button>TELL ME MORE</button>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
