77f9ef56d83a4a7aef3697acf721ea856654bcf1
jaidan22 <jaidansleeba22@gmail.com>
2022-02-12 21:59:53 +0530

    updated contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./contact.scss";
-import homeBg from "../../img/map-imgaage.png";
 
 const Contact = () => {
   return (
     <div className="contact">
-      <div className="wrapper">
-        <h2>Welcome To Our Studio!</h2>
-        <h1>IT'S NICE TO MEET YOU</h1>
-        <button>TELL ME MORE</button>
+      <div className="ctct-head">
+        <span class="heading">CONTACT US</span>
+        <span className="desc">Lorem ipsum dolor sit amet consectetur.</span>
       </div>
+      <form action="post">
+        <div className="left">
+          <input type="text" required placeholder="Your Name *" />
+          <input type="email" required placeholder="Your Email *" />
+          <input type="tel" required placeholder="Your Phone *" />
+        </div>
+        <div className="right">
+          <textarea name="message" id="message" placeholder="Your Message *" />
+        </div>
+        <button type="submit">MESSAGE</button>
+      </form>
     </div>
   );
 };
