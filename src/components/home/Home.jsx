1e6c85c640d9a60b79437a2cce5ab382f4183cd9
jaidan22 <jaidansleeba22@gmail.com>
2022-02-12 09:54:42 +0530

    added home

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./home.scss";
+import homeBg from "../../img/header-bg.jpg";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="wrapper">
+        <h2>Welcome To Our Studio!</h2>
+        <h1>IT'S NICE TO MEET YOU</h1>
+        <button>TELL ME MORE</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
